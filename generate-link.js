82392709ff217f4e3915e2a17c5f1b37e6723d7c
rinocomonster <41083098+rinocomonster@users.noreply.github.com>
2019-12-03 16:17:37 +0700

    Add files via upload

---
+++ generate-link.js
@@ -0,0 +1,9 @@
+var timeleft = 10;
+var downloadTimer = setInterval(function(){
+  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "[download_link]"
+  }
+}, 1500);
