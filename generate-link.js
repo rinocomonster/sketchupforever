bcf42e94605d4bf30da9f7c66d2b74359364bea4
rinocomonster <41083098+rinocomonster@users.noreply.github.com>
2019-12-06 13:12:43 +0700

    Add files via upload

---
+++ generate-link.js
@@ -0,0 +1,10 @@
+var timeleft = 10;
+var downloadTimer = setInterval(function(){
+  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Scroll down to get Download link"
+    document.getElementById("btndownload").removeAttribute("style");
+  }
+}, 1500);
